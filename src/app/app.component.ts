3cf4a06cf1e9c429a284367523f70131feb42591
brunannunes <brunanunes324@gmail.com>
2024-05-16 10:27:43 -0300

    consumindo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CommonModule } from '@angular/common';
 import {FormsComponent} from './components/forms/forms.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
