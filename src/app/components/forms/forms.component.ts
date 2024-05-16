4ebb26e31d12afa6632d6fd81ad6f767ee18df9e
brunannunes <brunanunes324@gmail.com>
2024-05-16 11:25:30 -0300

    esta com erro

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
-
+import { Location } from '../../types/location.interface';
 
 
 
@@ -14,7 +14,9 @@ import { GetUnitsService } from '../../services/get-units.service';
 })
 export class FormsComponent implements OnInit{
   
-  results = [];  
+  //tipando o results para ser do tipo location array
+  results: Location[] = [];  
+  filtredResults: Location[] = [];
   //  ao colocarmos o "!" estamos 
   // dizendo que já vamos fazer a declaração desse atributo
   formGroup!: FormGroup;
@@ -23,15 +25,25 @@ export class FormsComponent implements OnInit{
   
   
   ngOnInit(): void {
-    this.unitService.getAllUnits().subscribe(data => console.log(data));
     this.formGroup = this.formBuilder.group({
       hour: '',
       showClosed: false
     })
+    this.unitService.getAllUnits().subscribe(data => {
+      //results recebe data
+      this.results = data.locations;
+      this.filtredResults = data.locations;
+    });
   }
 
     onSubmit(): void{
       console.log(this.formGroup.value)
+      if(!this.formGroup.value.showClosed){
+        console.log("entreou")
+        this.filtredResults = this.results.filter(location => location.opened === true)
+      }else{
+        this.filtredResults = this.results
+      }
     }
 
     onClean(): void{
