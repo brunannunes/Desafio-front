322aaaa936fa9ebb455c7201d6dc7ffd8e4178c5
brunannunes <brunanunes324@gmail.com>
2024-05-22 11:20:03 -0300

    incompleto

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -2,23 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
 import { Location } from '../../types/location.interface';
+import { FilterUnitsService } from '../../services/filter-units.service';
 
-const OPENING_HOURS = {
-  morning: {
-    first: '06',
-    last: '12'
-  },
-  afternoon: {
-    first: '12',
-    last: '18'
-  },
-  night: {
-    first: '18',
-    last: '23'
-  }
-}
 
-type HOUR_INDEXES = 'morning' | 'afternoon' | 'night'
+
 
 
 
@@ -39,7 +26,10 @@ export class FormsComponent implements OnInit{
   // dizendo que já vamos fazer a declaração desse atributo
   formGroup!: FormGroup;
   
-  constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) {}
+  constructor(
+    private formBuilder: FormBuilder, 
+    private unitService: GetUnitsService, 
+    private filterUnitsService: FilterUnitsService) {}
   
   
   ngOnInit(): void {
@@ -55,56 +45,11 @@ export class FormsComponent implements OnInit{
   }
 
     // fazendo um switch case para ele retornar os dias da semana de acordo com o numero
-    transformWeekday(weekday: number){
-      switch (weekday) {
-        case 0:
-          return 'Dom.'
-        case 6:
-          return 'Sáb.'
-        default:
-          return 'Seg. à Sex.'
-      }
-    }
-    filterUnits(unit: Location, open_hour: string, close_hour: string) {
-      if(!unit.schedules) return true;
   
-      let open_hour_filter = parseInt(open_hour, 10)
-      let close_hour_filter = parseInt(close_hour, 10)
-  
-      let todays_weekday = this.transformWeekday(new Date().getDay());
-  
-      for(let i = 0; i < unit.schedules.length; i++){
-        let schedule_hour = unit.schedules[i].hour
-        let schedule_weekday = unit.schedules[i].weekdays
-        if(todays_weekday === schedule_weekday){
-          if(schedule_hour !== 'Fechada'){
-            let [unit_open_hour, unit_close_hour] = schedule_hour.split(' às ')
-            let unit_open_hour_int = parseInt(unit_open_hour.replace('h', ''), 10)
-            let unit_close_hour_int = parseInt(unit_close_hour.replace('h', ''), 10)
-  
-            if(unit_open_hour_int <= open_hour_filter && unit_close_hour_int >= close_hour_filter) return true
-            else return false
-          }
-        }
-      }
-  
-      return false;
-    }
     onSubmit(): void{
-      let intermidiateResults = this.results;
-      
-      if(!this.formGroup.value.showClosed){
-        intermidiateResults = this.results.filter(location => location.opened === true)
-      }
-      
-      if(this.formGroup.value.hour){
-        const OPEN_HOUR = OPENING_HOURS[this.formGroup.value.hour as HOUR_INDEXES].first
-        const CLOSE_HOUR = OPENING_HOURS[this.formGroup.value.hour as HOUR_INDEXES].last
-        this.filtredResults = intermidiateResults.filter(location => this.filterUnits(location, OPEN_HOUR, CLOSE_HOUR));
-      }else{
-        this.filtredResults = intermidiateResults;
-      }
-        console.log(this.filtredResults)
+      let {showClosed, hour} = this.formGroup.value
+      this.filtredResults = this.filterUnitsService.filter(this.results, showClosed,hour)
+        
     }
 
     onClean(): void{
--- src/app/services/get-units.service.ts
+++ src/app/services/get-units.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { UnitsResponse } from '../types/units-response.interface';
+import { Location } from '../types/location.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,22 @@ import { UnitsResponse } from '../types/units-response.interface';
 export class GetUnitsService {
   readonly apiUrl = "https://test-frontend-developer.s3.amazonaws.com/data/locations.json";
 
-  constructor(private httpClient: HttpClient) { }
+  //um objeto behavior subject irá mudar de estado
+  private allUnitsSubject: BehaviorSubject<Location[]> = new BehaviorSubject<Location[]>([]);
+  //temos tambem um objeto que é observavel que notifica os outros que o seu valor mudou
+  private allUnits$: Observable<Location[]> = this.allUnitsSubject.asObservable();
+  private filteredUnits: Location[] = [];
+
+  constructor(private httpClient: HttpClient) {
+   this.httpClient.get<UnitsResponse>(this.apiUrl).subscribe(data => {
+    //results recebe data
+    this.results = data.locations;
+    this.filtredResults = data.locations;
+
+   }
 
   //criando um metodo para poder pegar as unidades
   getAllUnits(): Observable<UnitsResponse>{
-    return this.httpClient.get<UnitsResponse>(this.apiUrl)
+    return
   }
 }
