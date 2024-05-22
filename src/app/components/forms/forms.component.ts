bbe1a9076cc0a2fcc4ffc69452aef0f25db8a761
brunannunes <brunanunes324@gmail.com>
2024-05-22 08:47:31 -0300

    loop

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -3,6 +3,24 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
 import { Location } from '../../types/location.interface';
 
+const OPENING_HOUR={
+    morning:{
+      first: '06',
+      last: '12'
+    },
+    afternoon:{
+      first: '12',
+      last: '18'
+    },
+    night:{
+      first: '18',
+      last: '23'
+    }
+
+}
+
+type HOUR_INDEXES = 'morning' | 'afternoon' | 'night'
+
 
 
 @Component({
@@ -36,10 +54,23 @@ export class FormsComponent implements OnInit{
     });
   }
 
+    filterUnits(units:Location, opened: boolean, open_hour: string, close_hour:string){
+      let open_hour_filter = parseInt(open_hour, 10)
+      let close_hour_filter = parseInt(close_hour, 10)
+
+      //pega o dia atual da semana
+      let today_weekday = new Date().getDay();
+
+      //loop entre os dias da unidade para retornar os dias em numeros
+      for(let i = 0; i < units.schedules.length; i ++){
+        let schedule_hour = units.schedules[i].hour
+        let schedule_weekday = units.schedules[i].weekdays
+      }
+    }
     onSubmit(): void{
-      console.log(this.formGroup.value)
+      const OPEN_HOUR = OPENING_HOUR[this.formGroup.value.hour as HOUR_INDEXES].first
+      const CLOSE_HOUR = OPENING_HOUR[this.formGroup.value.hour as HOUR_INDEXES].last
       if(!this.formGroup.value.showClosed){
-        console.log("entreou")
         this.filtredResults = this.results.filter(location => location.opened === true)
       }else{
         this.filtredResults = this.results
