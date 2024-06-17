fd58f06a51b7a82fc6b8de3e2f9a24a1796d7220
brunannunes <brunanunes324@gmail.com>
2024-06-17 15:28:23 -0300

    estilização

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { GetUnitsService } from '../../services/get-units.service';
 import { Location } from '../../types/location.interface';
 import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, CommonModule],
   templateUrl: './card-list.component.html',
   styleUrl: './card-list.component.scss'
 })
